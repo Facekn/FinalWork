7150b200a464cbbfcdee81d06089c03979fdb754
Ilya Grigorev <facekn110388@gmail.com>
2022-11-29 15:28:44 +0300

    Добавил код программы

---
+++ ProgramCode/Program.cs
@@ -0,0 +1,13 @@
+﻿void SelectArray (string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length<=3)
+        Console.Write (array[i] + " ");
+        else
+        Console.Write ("");
+    }
+}
+
+string [] array = {"hello", "2", "world", ":-)", "123", "no", "yes", "stop", "product", "git"};
+SelectArray (array);
