cf05744ee8374fbfa4a93a75b32918f384b3e98f
Ilya Grigorev <facekn110388@gmail.com>
2022-12-01 23:29:53 +0300

    Добавил исправленный вариант кода

---
+++ CorrectedFinalWork/Program.cs
@@ -0,0 +1,27 @@
+﻿void CreatedArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void ResultingArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Console.Write("Введите элементы массива через пробел: ");
+string[] array = Console.ReadLine().Split();
+string[] array2 = new string[array.Length];
+CreatedArray(array, array2);
+Console.WriteLine();
+Console.Write("Получившийся массив с элементами <= 3 символов: ");
+ResultingArray(array2);
